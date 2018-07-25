c19fb904c4182793c3d40f4201978e3dc0910fd4
gchu <gchu647@gmail.com>
2018-07-24 20:20:13 -1000

    POST, PUT, DELETE methods working

--- app.js
+++ app.js
@@ -11,23 +11,57 @@ app.get('/buzzwords', (req, res) => {
   if(buzzArr.length > 0) {
     let allBuzzWords = {}
     //Make an object with just the buzzWords
-    allBuzzWords['buzzWord'] = buzzArr.map((element) =>{ return element['buzzWord']});
+    allBuzzWords.buzzWord = buzzArr.map((element) =>{ return element.buzzWord});
 
     res.json(allBuzzWords);
   } else {
     res.status(400).send('No buzzWords. Add some in with POST');
   }
-})
+});
 
-app.post('/buzzwords', (req, res) => {
+app.post('/buzzwords', (req, res) => {// Shouldn't allow you to post the same word
   if(buzzArr.length <= 5) {
     buzzArr.push(req.body);
-    res.send('Your data has been created!');
+    res.json({ "success": true });
   } else {
-    res.status(400).send('POST error! Reached max buzzwords length');
+    res.status(400).json({ "success": false });
   }
 
   console.log(buzzArr);
+});
+
+app.put('/buzzwords', (req, res) => {
+  let change = false; //Checks if I changed something
+
+  buzzArr.forEach((element) => {
+    if(element.buzzWord === req.body.buzzWord) {
+      element.points = req.body.points;
+      change = true;
+    }
+  });
+
+  if(change) {
+    res.json({ "success": true });
+  } else {
+    res.status(400).json({ "success": false });
+  }
+});
+
+app.delete('/buzzwords', (req, res) => {
+  let remove = false;
+
+  buzzArr.forEach((element, index) => {
+    if(element.buzzWord === req.body.buzzWord) {
+      buzzArr.splice(index, 1);
+      remove = true;
+    }
+  });
+
+  if(remove) {
+    res.json({ "success": true });
+  } else {
+    res.status(400).json({ "success": false });
+  }
 });
 
 app.listen(PORT, () => {
