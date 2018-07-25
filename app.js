db436a029ba247e02eb5b6377504e13d7b90460b
gchu <gchu647@gmail.com>
2018-07-24 19:16:46 -1000

    Can GET an object of buzzwords

--- app.js
+++ app.js
@@ -2,20 +2,32 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 8060;
-const buzzWordArr = [];
+const buzzArr = [];
 
 app.use(express.static('./public'));
 app.use( bodyParser.urlencoded( {extended: true})); //encodes the req
 
+app.get('/buzzwords', (req, res) => {
+  if(buzzArr.length > 0) {
+    let allBuzzWords = {}
+    //Make an object with just the buzzWords
+    allBuzzWords['buzzWord'] = buzzArr.map((element) =>{ return element['buzzWord']});
+
+    res.json(allBuzzWords);
+  } else {
+    res.status(400).send('No buzzWords. Add some in with POST');
+  }
+})
+
 app.post('/buzzwords', (req, res) => {
-  if(buzzWordArr.length <= 5) {
-    buzzWordArr.push(req.body);
+  if(buzzArr.length <= 5) {
+    buzzArr.push(req.body);
     res.send('Your data has been created!');
   } else {
     res.status(400).send('POST error! Reached max buzzwords length');
   }
 
-  console.log(buzzWordArr);
+  console.log(buzzArr);
 });
 
 app.listen(PORT, () => {
