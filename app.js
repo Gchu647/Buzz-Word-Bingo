48fff80bce55d6503049747cf883435a217f2010
gchu <gchu647@gmail.com>
2018-07-24 18:38:59 -1000

    Finish POST method

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-
 const PORT = process.env.PORT || 8060;
+const buzzWordArr = [];
 
 app.use(express.static('./public'));
+app.use( bodyParser.urlencoded( {extended: true})); //encodes the req
 
 app.post('/buzzwords', (req, res) => {
-  
+  if(buzzWordArr.length <= 5) {
+    buzzWordArr.push(req.body);
+    res.send('Your data has been created!');
+  } else {
+    res.status(400).send('POST error! Reached max buzzwords length');
+  }
+
+  console.log(buzzWordArr);
 });
 
 app.listen(PORT, () => {
